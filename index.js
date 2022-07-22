4ec4670e9a15f953312dcd7e160444a01b19b06a
Barrett Hiltabrand <bear2927@yahoo.com>
2022-07-22 12:32:06 -0500

    commit tree 0143ff950b955303338004daa62081a102ce425f

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
+
 // Code your solution here
+const totalBatteries = batteryBatches.length
+const init = 0
+let sum = batteryBatches.reduce(function (total, battery) {
+    return total + battery;
+}, init);
+
+
+// const sumBatteries = reduce(batteryBatches, function (num) {
+//     return (num + num);
+// });
